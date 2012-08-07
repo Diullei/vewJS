d3470667be843e6778ccfe348258b91147b0853b
Daniel Filho <daniel.filho@gmail.com>
2012-08-07 16:23:27 -0300

    retirando .js desnecessario

--- vewjs.bookmarklet.js
+++ vewjs.bookmarklet.js
@@ -1,1 +1,22 @@
-javascript:(function(){function r(){var e=document.createElement('script');e.src='https://raw.github.com/danielfilho/vewJS/dev/vew.js',document.getElementsByTagName('head')[0].appendChild(e)}var e='1.4.4';if(window.jQuery===undefined||window.jQuery.fn.jquery<e){var t=!1,n=document.createElement('script');n.src='https://ajax.googleapis.com/ajax/libs/jquery/'+e+'/jquery.min.js',n.onload=n.onreadystatechange=function(){!t&&(!this.readyState||this.readyState=='loaded'||this.readyState=='complete')&&(t=!0,r())},document.getElementsByTagName('head')[0].appendChild(n)}else r()})()
+(function(){
+  var v     = '1.4.4';
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done      = false;
+    var script    = document.createElement('script');
+    script.src    = 'https://ajax.googleapis.com/ajax/libs/jquery/'+ v +'/jquery.min.js';
+    script.onload = script.onreadystatechange = function(){
+      if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+        done = true;
+        initMyBookmarklet();
+      }
+    };
+    document.getElementsByTagName('head')[0].appendChild(script);
+  } else {
+    initMyBookmarklet();
+  }
+  function initMyBookmarklet() {
+    var script    = document.createElement('script');
+    script.src    = 'https://raw.github.com/danielfilho/vewJS/dev/vew.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
+})();
